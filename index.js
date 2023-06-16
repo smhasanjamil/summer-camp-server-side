d948bc11712ce61e081f97f1cdd4da2536390666
smhasanjamil <smhasanjamil23@gmail.com>
2023-06-16 18:52:54 +0600

    Delete card items

--- index.js
+++ index.js
@@ -241,6 +241,21 @@ async function run() {
 
 
 
+        // Delete cart items
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+
+
+
+
+
 
 
         // Connect the client to the server	(optional starting in v4.7)
